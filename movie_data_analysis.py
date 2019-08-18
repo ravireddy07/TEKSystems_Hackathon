1cb89cdc79d056ee60f7adc6db553473fb136068
sasank <kovid.dattu1999@gmail.com>
2019-08-18 13:09:28 +0530

    linear regression

--- movie_data_analysis.py
+++ movie_data_analysis.py
@@ -64,3 +64,11 @@ def predict(self):
         z = pd.read_csv('./trend_emotion.csv')
         X = z.iloc[:, :-1]
         y = z.iloc[:, -1]
+         # Spliting Data 
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+        # Linear Regression
+        from sklearn.linear_model import LinearRegression
+        lm = LinearRegression()
+        lm.fit(X_train, y_train)
+        predictions_lin = lm.predict(X_test)
