919b03fecbbe83f258bfb36269f7a3b6a5bd23b8
sasank <kovid.dattu1999@gmail.com>
2019-08-18 13:32:26 +0530

    printing the output vector

--- movie_data_analysis.py
+++ movie_data_analysis.py
@@ -46,19 +46,9 @@ class NLP:
                         print("Query does not contain enough parameters.")
                         return self.processing()
         self.output.append(self.number_of_constraints)
+        for q in range(len(self.output_vectors)):
+            for value,vectors in token_outputs.items():
+                if (self.output_vectors[q]==vectors):
+                    self.output.append(value)
         
-def predict(self):
-        df_area = self.trends(False)
-        print(df_area)
-        # Data-Preprocessing
-        z = pd.read_csv('./trend_emotion.csv')
-        X = z.iloc[:, :-1]
-        y = z.iloc[:, -1]
-         # Spliting Data 
-        from sklearn.model_selection import train_test_split
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-        # Linear Regression
-        from sklearn.linear_model import LinearRegression
-        lm = LinearRegression()
-        lm.fit(X_train, y_train)
-        predictions_lin = lm.predict(X_test)
+
